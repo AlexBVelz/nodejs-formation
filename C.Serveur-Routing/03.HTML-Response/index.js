dec847621ffcf36376cd62e520493b927ba1cfc7
HyunfixFR <velzogfr@gmail.com>
2025-01-21 19:03:38 +0100

    feat(nodejs-formations): add json response and create serveur

---
+++ C.Serveur-Routing/03.HTML-Response/index.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const fs = require('fs');
+
+
+const PORT = 3000;
+const FILE_PATH = 'index.html';
+
+
+const server = http.createServer((req,res) => {
+    fs.readFile(FILE_PATH, 'utf8', (err,data) => {
+        if(err){
+            res.writeHead(500, {"Content-Type": "text/plain"});
+            res.end("Erreur interne du serveur");
+            return;
+        }
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(data);
+    })
+})
+
+server.listen(PORT, ()=> {
+    console.log(`Le serveur est à l'écoute sur le port ${PORT}`);
+})
